df3140c4dc1de8739255fbfdc618e60cab9af150
Patricia Bernadette Aspillaga <144974708+TriciaBAA@users.noreply.github.com>
2025-03-17 04:27:32 +0000

    added initial routes

--- Finals/resources/views/welcome.blade.php
+++ Finals/resources/views/welcome.blade.php
@@ -1,10 +1,9 @@
-/**test page */
 <!doctype html>
 <html>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> /**test only can delete later */
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> 
     @vite('resources/css/app.css')
   </head>
   <body>
--- Finals/routes/web.php
+++ Finals/routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+use App\Http\Controllers\GalleryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+//for testing tailwind
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', [GalleryController::class, 'show']);
